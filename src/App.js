5d2645184ff22372592389274b708e05ba857360
Dominic Law <dominic.law79@gmail.com>
2021-10-28 16:58:55 +1100

    Removed router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Login from "./Login"
-import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <Router>
       <div className="App" class="has-background-light">
         <Login />
       </div>
-    </Router>
   );
 }
 
