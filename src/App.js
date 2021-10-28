fce288cdf81b90d6fc945064679311a0a0c12886
Dominic Law <dominic.law79@gmail.com>
2021-10-28 16:03:34 +1100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Login from "./Login"
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App" class="has-background-light">
-      <Login />
-    </div>
+    <Router>
+      <div className="App" class="has-background-light">
+        <Login />
+      </div>
+    </Router>
   );
 }
 
